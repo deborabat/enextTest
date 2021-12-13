d3188ab3de69ce3b6d4df042072829a1292826eb
deborabat <deehsilva461@gmail.com>
2021-12-12 22:36:57 -0300

    fix: cors error, open modal and hange function getPotions

--- src/scripts/Home/Index.js
+++ src/scripts/Home/Index.js
@@ -1,14 +1,12 @@
 function openModal(valor){
 
   const modal = document.getElementById("white-box");
-
   modal.style.display = "block";
-  product(valor)
-  console.log(valor);
-  
+  getPotions(valor)
+    
 }
 
-function closeModal(valor){
+function closeModal(){
 
   const modal = document.getElementById("white-box");
   modal.style.display = "none";
@@ -19,46 +17,30 @@ function closeModal(valor){
       modal.style.display = "none";
     }
   }
+  }
 
-  console.log("closed");
-}
+async function getPotions(produto){
+  const response = await fetch('http://localhost:3000/potions', 
+    {   method: 'GET',
+    })
+ 
+  const obj =  await response.json();
+  const product = obj[produto];
+  const combo = product.ingredients;
 
-function loadJSON(callback) {   
-
-var xobj = new XMLHttpRequest();
-xobj.overrideMimeType("application/json");
-xobj.open('GET', '../../../assets/potions.json', true);
-xobj.onreadystatechange = function () {
-  if (xobj.readyState == 4 && xobj.status == "200") {
-    callback(xobj.responseText);
-    }
-};
-xobj.send(null);  
-}
-function product(produto){
-loadJSON(function(err) {
-  var obj = JSON.parse(err);
-    
-  var product = obj.potions[produto];
-  var combo = product.ingredients;
-
-  console.log(product.ingredients);
-    
-  var i;
-  var img = document.getElementById("product-box");
-  var nome = document.getElementById("nome");
-  var effect = document.getElementById("effect");
-  var igredients = document.getElementById("igredients");
+  const img = document.getElementById("product-box");
+  const nome = document.getElementById("nome");
+  const effect = document.getElementById("effect");
+  const igredients = document.getElementById("igredients");
   igredients.innerHTML = "";
-  var price = document.getElementById("price");
-    
+  const price = document.getElementById("price");
 
-  img.src = 'img/products/'+ product.image;
+  img.src = '../../../assets/productos/'+ product.image;
   nome.innerHTML = product.name;
   effect.innerHTML = product.effect;
-    for (i = 0; i < combo.length; i++) { 
-        igredients.innerHTML += product.ingredients[i]+"<br/>";
-    }
-    price.innerHTML = "$"+ product.price;
-  });
+  for (let i = 0; i < combo.length; i++) { 
+    igredients.innerHTML += product.ingredients[i]+"<br/>"
   }
+  price.innerHTML = "$"+ product.price;
+  
+}
