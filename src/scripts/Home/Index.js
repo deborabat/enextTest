1d0d14371aea82a0fd3361d14bddfc5618ece258
deborabat <deehsilva461@gmail.com>
2021-12-21 14:35:23 -0300

    feat: add menu responsive

--- src/scripts/Home/Index.js
+++ src/scripts/Home/Index.js
@@ -85,3 +85,13 @@ const setListPotions = (obj) => {
   }
   
 }
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "90%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
